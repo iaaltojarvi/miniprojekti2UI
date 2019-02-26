ee5ebf29078b7c169905d579af4caf3e05478438
Juhanir <juhani.riisio@gmail.com>
2019-02-26 13:51:26 +0200

    made more components

--- src/components/Selectionview/Categories.js
+++ src/components/Selectionview/Categories.js
@@ -1,11 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
+import './Categories.css';
 
 export default class Categories extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <Container className="topic-container">
+        <Row>
+          <Col>
+            <ListGroup>
+              <ListGroup.Item onClick={() => alert('All')}>All categories</ListGroup.Item>
+              <ListGroup.Item onClick={() => alert('Javascript')}>Javascript</ListGroup.Item>
+              <ListGroup.Item onClick={() => alert('Node')}>NodeJS</ListGroup.Item>
+              <ListGroup.Item onClick={() => alert('ReactJS')}>ReactJS</ListGroup.Item>
+              <ListGroup.Item onClick={() => alert('Database')}>Databases</ListGroup.Item>
+            </ListGroup>
+          </Col>
+        </Row>
+      </Container>
     )
   }
 }
