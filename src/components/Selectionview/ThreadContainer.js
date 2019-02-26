8c00b951deb15128fe64ef9da77893cf4f575cc7
Juhanir <juhani.riisio@gmail.com>
2019-02-26 15:50:35 +0200

    added thread card prototypes

--- src/components/Selectionview/ThreadContainer.js
+++ src/components/Selectionview/ThreadContainer.js
@@ -1,41 +1,27 @@
 import React, { Component } from 'react'
 import Container from 'react-bootstrap/Container';
+import Thread from './Thread';
+
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+const createId = () => {
+  return (Math.random() * Date.now()).toFixed(0);
+}
+
+
 export default class ThreadContainer extends Component {
   render() {
+    const threadList = Array(6).fill({ author: "Groot!", text: "Lorem ipsum dolor sit amet consectetur adipisicing elit!" });
+    const newList = threadList.map(thread => {
+      return (<Col lg={4} key={createId()}><Thread author={thread.author} text={thread.text}></Thread></Col>);
+    });
     return (
       <Container>
         <Row>
-          <Col lg={3}>
-            Contents!
-          </Col>
-          <Col lg={3}>
-            Contents!
-          </Col>
-          <Col lg={3}>
-            Contents!
-          </Col>
-          <Col lg={3}>
-            Contents!
-          </Col>
-        </Row>
-        <Row>
-          <Col lg={3}>
-            Contents!
-          </Col>
-          <Col lg={3}>
-            Contents!
-          </Col>
-          <Col lg={3}>
-            Contents!
-          </Col>
-          <Col lg={3}>
-            Contents!
-          </Col>
+          {newList}
         </Row>
       </Container>
-    )
+    );
   }
 }
