094545504555b306612a912f13efa4ffe7d356fd
Juhanir <juhani.riisio@gmail.com>
2019-02-28 13:14:28 +0200

    got signup to work

--- src/components/appNavigation/SignupForm.js
+++ src/components/appNavigation/SignupForm.js
@@ -1,33 +1,61 @@
 import React, { Component } from 'react';
-//import { createquote } from "../serviceclient";
 
 class SignupForm extends Component {
-    state = {email: "", password: ''};
+    constructor(props) {
+        super(props);
+        this.state = { username: '', password: '' };
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
     handleEmailChange = (e) => {
         const uusiarvo = e.target.value;
-        this.setState({email: uusiarvo});
+        this.setState({ username: uusiarvo });
     }
     handlePasswordChange = (e) => {
         const uusiarvo = e.target.value;
-        this.setState({password: uusiarvo});
+        this.setState({ password: uusiarvo });
     }
-    handleCreateClick = (e) => {
+
+    handleSubmit = async (e) => {
         e.preventDefault();
-    }
-
-
-    render() {
-        return(
-            <form>
-                <input type="text" placeholder="Email" 
-               value={this.state.email} onChange={this.handleEmailChange}/><br />
-                <input type="text" placeholder="Password" 
-               value={this.state.password} onChange={this.handlePasswordChange}/><br />
-                <input type="submit" value="Create Account" onClick={this.handleCreateClick}/>
-            </form>
-
-        );
+        if (!this.state.username || !this.state.password) {
+            alert('Please fill both username and password');
+        }
+        try {
+            let response = await fetch('/api/users', {
+                mode: 'cors',
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(this.state)
+            });
+            let jsonRes = await response.json();
+            if (jsonRes.success) {
+                alert('' + jsonRes.msg);
+                this.props.history.push('/');
+            } else {
+                throw new Error();
+            }
+        } catch (error) {
+            console.log('User creation failed!');
+            this.setState({ username: '', password: '' });
         }
     }
+
+    render() {
+        return (
+            <form>
+                <input type="text" placeholder="Username"
+                    value={this.state.email} onChange={this.handleEmailChange} /><br />
+                <input type="password" placeholder="Password"
+                    value={this.state.password} onChange={this.handlePasswordChange} /><br />
+                <input type="submit" value="Create Account" onClick={this.handleSubmit} />
+            </form>
+        );
+    }
+}
 
 export default SignupForm;
